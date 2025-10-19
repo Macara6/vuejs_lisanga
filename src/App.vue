d1646f5c9468bdde4da6973fbc20cc19b83758d7
Reginald macara <reginadmacara16@gmail.com>
2025-10-20 01:44:35 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
+
+</script>
+
 
 <template>
+     
     <router-view />
+    <Analytics />
 </template>
 
 <style scoped></style>
